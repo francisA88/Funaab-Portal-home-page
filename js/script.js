4f0d947f6bf323ebeec1ad68d268185494571909
Francis Ali <githubemail61@gmail.com>
2020-10-19 22:49:58 +0100

    commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+collapsibles = document.querySelectorAll(".collapsible")
+M.Collapsible.init(collapsibles, {
+	accordion: false,
+})
+collheaders = document.querySelectorAll(".collapsible-header")
+collbodies = document.querySelectorAll(".collapsible-body")
+collbodies.forEach((elem)=>{
+	elem.style.border = "solid 0px";
+})
+collheaders.forEach((elem)=>{
+	elem.style.border = "solid 0px";
+})
+collapsibles.forEach((elem)=>{
+	elem.style.border = "solid 0px";
+})
+expand = document.querySelector("#expand")
+ch = collheaders[0]
+var chPressed = 1
+ch.addEventListener("click", function(e){
+	chPressed *= -1
+	if (chPressed === 1) expand.innerText = "chevron_right";
+	else if (chPressed === -1) expand.innerText = "expand_less";
+})
+scrollbtn = document.querySelector("#scrollbtn")
+scrollbtn.addEventListener("click", (e)=>{
+	window.scrollTo({
+		top: 0,
+		left: 0,
+		behavior: "smooth",
+	})
+})
